0bce0bbda2ad06b696868240264afcfeac4917e3
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-22 21:50:26 +0600

    searching functonality added done

--- src/pages/Home/Services/Service/Service.jsx
+++ src/pages/Home/Services/Service/Service.jsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServicesCard from "../ServicesCard/ServicesCard";
 import "./Service.css";
 
 const Service = () => {
   const [services, setServices] = useState([]);
   const [numServicesToShow, setNumServicesToShow] = useState(3);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
+
+  // ascending and descending state
+  const [asc, setAsc] = useState(true);
 
   const handleMoreServices = () => {
     setNumServicesToShow(numServicesToShow + 3);
@@ -13,10 +18,19 @@ const Service = () => {
   const visibleServices = services.slice(0, numServicesToShow);
 
   useEffect(() => {
-    fetch("https://car-doctor-server-gamma-pied.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc, search]);
+
+  // search handler
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div>
@@ -29,8 +43,39 @@ const Service = () => {
           technical support services. We are dedicated to delivering exceptional
           service and exceeding our clients expectations.{" "}
         </p>
+
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              ref={searchRef}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <button className="btn-discover" onClick={() => setAsc(!asc)}>
+          {asc ? "Make Price Low to Heigh" : "Make Price Heigh to Low"}
+        </button>
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-6">
+      <div className="md:grid md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-6 mb-10">
         {visibleServices.map((service) => (
           <ServicesCard key={service._id} service={service}></ServicesCard>
         ))}
